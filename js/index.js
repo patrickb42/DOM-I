7190783cef8f784931d797f1e9879c47eb52f8e5
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-17 18:06:09 -0400

    mvp

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const siteContent = {
   nav: {
+    'nav-item-0': 'Prepend',
     'nav-item-1': 'Services',
     'nav-item-2': 'Product',
     'nav-item-3': 'Vision',
     'nav-item-4': 'Features',
     'nav-item-5': 'About',
     'nav-item-6': 'Contact',
+    'nav-item-7': 'Obligation',
     'img-src': 'img/logo.png',
   },
   cta: {
@@ -40,3 +42,45 @@ const siteContent = {
 // Example: Update the img src for the logo
 const logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent.nav['img-src']);
+
+
+let newElementHolder = document.createElement('a');
+newElementHolder.href = '#';
+document.querySelector('nav').appendChild(newElementHolder);
+newElementHolder = document.createElement('a');
+newElementHolder.href = '#';
+document.querySelector('nav').prepend(newElementHolder);
+
+document.querySelectorAll('nav a').forEach((tag, index) => {
+  const element = tag;
+  element.textContent = siteContent.nav[`nav-item-${index}`];
+  element.style.color = 'green';
+});
+
+document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button;
+document.querySelector('#cta-img').src = siteContent.cta['img-src'];
+
+const topContent = document.querySelectorAll('.top-content .text-content');
+topContent[0].innerHTML = `<h4>${siteContent['main-content']['features-h4']}</h4>
+  <p>${siteContent['main-content']['features-content']}</p>`;
+topContent[1].innerHTML = `<h4>${siteContent['main-content']['about-h4']}</h4>
+  <p>${siteContent['main-content']['about-content']}</p>`;
+
+document.querySelector('#middle-img').src = siteContent['main-content']['middle-img-src'];
+
+const bottomContent = document.querySelectorAll('.bottom-content .text-content');
+bottomContent[0].innerHTML = `<h4>${siteContent['main-content']['services-h4']}</h4>
+  <p>${siteContent['main-content']['services-content']}</p>`;
+bottomContent[1].innerHTML = `<h4>${siteContent['main-content']['product-h4']}</h4>
+  <p>${siteContent['main-content']['product-content']}</p>`;
+bottomContent[2].innerHTML = `<h4>${siteContent['main-content']['vision-h4']}</h4>
+  <p>${siteContent['main-content']['vision-content']}</p>`;
+
+const contact = document.querySelector('.contact');
+contact.innerHTML = `<h4>${siteContent.contact['contact-h4']}</h4>
+  <p>${siteContent.contact.address}</p>
+  <p>${siteContent.contact.phone}</p>
+  <p>${siteContent.contact.email}</p>`;
+
+document.querySelector('footer p').textContent = siteContent.footer.copyright;
